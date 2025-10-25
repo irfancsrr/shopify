8119c8124f8a47b88976dd5eb86c902841d2c430
irfan <irfancsrrsimt@gmail.com>
2025-10-25 15:00:08 +0530

    adminUpdateForPopularSetting

--- admin/vite-project/src/Components/items/item.jsx
+++ admin/vite-project/src/Components/items/item.jsx
@@ -1,16 +1,10 @@
-import React from 'react'
-import { useEffect } from 'react';
-import { useState } from 'react';
 import './item.css'
-// import
+
 import remove_icon from "../../assets/remove_icon.png";
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
 
 const Item = ({product,Index}) => {
-    const [liked,setLiked]=useState(false);
-    const navigateTo=useNavigate();
-    const [productID,setProductID]=useState(null);
+    
 
 const removeproduct = async(id)=>{
       await fetch('https://back-end-1gp5.onrender.com/removeproduct',{
@@ -20,27 +14,31 @@ const removeproduct = async(id)=>{
           'Content-Type':'application/json',
         },body:JSON.stringify({id:id})
       })
-    //   navigateTo('/listproduct')
+    
     window.location.replace('/listproduct')
       
     }
 
-    useEffect(()=>{
-            if(liked){
-                fetch("https://back-end-1gp5.onrender.com/addpopuler", {
+   
+    const  handleAddPopular=(ID)=>{
+      fetch("https://back-end-1gp5.onrender.com/addpopuler", {
             method: "POST",
             headers: {
               Accept: "application/form-Data",
               "Content-Type": "application/json",
             },
-            body: JSON.stringify({ id: productID }),
+            body: JSON.stringify({ id: ID}),
           })
             .then((response) => response.json())
-            .then((data) => console.log(data));
-            }
-            
-        },[liked])
-        const  handleRemoveLiked=(ID)=>{
+            .then((data) => {console.log(data)
+              if(data.success)
+    window.location.replace('/listproduct')
+
+            });
+
+  
+    }
+        const  handleRemovePopular=(ID)=>{
        fetch("https://back-end-1gp5.onrender.com/removepopuler", {
         method: "POST",
         headers: {
@@ -50,10 +48,14 @@ const removeproduct = async(id)=>{
         body: JSON.stringify({ id: ID }),
       })
         .then((response) => response.json())
-        .then((data) => console.log(data));  
-    //  console.log("remove hu mai ");
-      // console.log(productID);
+        .then((data) => {console.log(data)
+          if(data.success)
+    window.location.replace('/listproduct')
+
+        });  
+  
     }
+    
   return (
     <div key={Index} className="listproduct-format-main listproduct-format" >
                 <img src={product.image}  className="listproduct-product-icon" />
@@ -62,12 +64,9 @@ const removeproduct = async(id)=>{
                 <p>${product.new_price}</p>
                 <p>{product.category}</p>
                 <img onClick={()=>{removeproduct(product.id)}} className='listproduct-remove-icon' src={remove_icon} />
-                <div style={{zIndex:"100",top:"10em"}} onClick={()=>{
-                                setLiked(!liked);
-                                
-                                setProductID(product._id);
-                                }}>
-                            {liked?<FaHeart color="red" size={24} onClick={()=>handleRemoveLiked(productID)} />:<FaRegHeart color="gray" size={24} />}       
+                <div style={{zIndex:"100",top:"10em",cursor:'pointer'}} >
+                            {product.popular?<FaHeart className='setPopular'  color="red" size={24} onClick={()=>handleRemovePopular(product._id)} />:
+                              <FaRegHeart className='setPopular' onClick={()=>handleAddPopular(product._id)} color="gray" size={24} />}       
                             
                 
                             </div>
